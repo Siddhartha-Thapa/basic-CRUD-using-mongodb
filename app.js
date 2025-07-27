6788a2f2ac97777bb1da82759f2b821328611f99
Siddhartha Thapa <siddharthathapa001@gmail.com>
2025-07-27 22:04:47 +0545

    some changes

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const usermodel = require('./usermodel');
 
 app.get('/', function(req, res){
     res.send("hello");
 });
+app.get('/create', async(req, res)=>{
+    let createduser = await usermodel.create({
+        name: "Kamal",
+        username: "kamal123",
+        email: "kamal@gmail.com"
+    
+    });
+    res.send(createduser);
+});
+
 
 app.listen("3000");
